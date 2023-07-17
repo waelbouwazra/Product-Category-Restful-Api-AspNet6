8f0a7bd011ebf2cdc0e91e1210d21dc703f459c5
waelbouwazra <98458757+waelbouwazra@users.noreply.github.com>
2023-07-17 07:49:51 +0200

    Application Core

---
+++ ProductCategory.Core/Dtos/CategoryDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCategory.Core.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? Label { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public string? Code { get; set; }
+    }
+    public class CategoryCreationDto : CategoryAddUpdateDto
+    {
+        
+    }
+
+    public class CategoryUpdateDto : CategoryAddUpdateDto { }
+    public class CategoryAddUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string? Label { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public string? Code { get; set; }
+    }
+}
